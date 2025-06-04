1c9c8aed7e16361d2c06f539d9155db85c867bfa
Kshitij Kumar <kshitijkumarchoudhary@gmail.com>
2025-06-04 05:59:26 +0530

    Exception handling for correctly parsing the ltpElement value

--- stocks.js
+++ stocks.js
@@ -81,7 +81,7 @@ const addOrUpdatePerformanceOnAmountChange = async (amount) => {
     } else {
         amount = amountElement.value;
     }
-    const ltpValue = parseFloat(ltpElement.textContent.replace(/₹/g, ''));
+    const ltpValue = parseFloat(ltpElement.textContent.replace(/[₹,]/g, ''));
     const qtyValue = parseInt(amount / ltpValue);
     if (!quantityElement) {
         quantityElement = document.createElement('div');
@@ -123,7 +123,7 @@ const addInputFields = async () => {
 const addOrUpdatePerformanceOnLtpChange = async () => {
     if (highElement) {
         const highValue = parseFloat(highElement.textContent.replace(/,/g, ''));
-        const ltpValue = parseFloat(ltpElement.textContent.replace(/₹/g, ''));
+        const ltpValue = parseFloat(ltpElement.textContent.replace(/[₹,]/g, ''));
         const ltpHighPercentageDiff = (((highValue - ltpValue) / ltpValue) * 100).toFixed(2);
 
         if (!performanceLtpHighValue) {
